e177fc25a6b62ce99991eb5ebd8cbf9ce8b1a6d0
salonibansod <147966433+salonibansod@users.noreply.github.com>
2025-01-02 12:19:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+describe('Signup Modal Test', () => {
+  it('should open and close the modal', () => {
+    
+    cy.get('button').contains('Sign Up').click();
 
-//your JS code here. If required.
+
+    cy.get('.modal').should('be.visible')
+    cy.get('.btn-close').click();
+
+
+    cy.get('.modal').should('not.be.visible');
+  });
+});
+
+
